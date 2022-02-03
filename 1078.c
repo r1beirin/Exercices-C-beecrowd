baeb15c10b0795512463510368043d4f382fc5e5
Mateus <77507023+r1beirin@users.noreply.github.com>
2022-02-03 19:09:00 -0300

    Create 1078.c

---
+++ 1078.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n, i = 1;
+    
+    scanf("%d", &n);
+
+    for(i; i <= 10; i++){
+        printf("%d x %d = %d\n", i, n, i*n);
+    }
+
+    return 0;
+}
