7d90ee2e978318be79a714ca5a2ad8d7077a5fba
Mateus <77507023+r1beirin@users.noreply.github.com>
2022-02-16 09:00:19 -0300

    Create 1180.c

---
+++ 1180.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	int n, i, menor, pos;
+
+	scanf("%d", &n);
+	int x[n];
+
+	for(i = 0; i < n; i++){
+		scanf("%d", &x[i]);
+		if(i == 0){
+			menor = x[i];
+			pos = i;
+		}
+
+		if(x[i] < menor){
+			menor = x[i]; pos = i;
+		}
+	}
+
+	printf("Menor valor: %d\n", menor);
+	printf("Posicao: %d\n", pos);
+
+	return 0;
+}
