9dad6a21a5e2ce9bf8f051ecf8fda757af4ab1bd
Mateus <77507023+r1beirin@users.noreply.github.com>
2022-02-16 09:03:40 -0300

    Create 1183.c

---
+++ 1183.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define MAX 12
+
+int main(){
+	int i, j, k;
+	double m[MAX][MAX], soma = 0, media = 0, cont = 0;
+	char t;
+
+	scanf("%c", &t);
+
+	for(i = 0; i < MAX; i++){
+		for(j = 0; j < MAX; j++){
+			scanf("%lf", &m[i][j]);
+		}
+	}
+	
+	for(i = 0; i < MAX; i++){
+		for(j = 0; j < MAX; j++){
+			if(i == j){
+				for(k = i; k < MAX; k++){
+					soma += m[i][k];
+					cont++;
+				}
+				soma -= m[i][j];
+				cont--;
+			}
+		}
+	}
+
+	if(t == 'S') printf("%.1lf\n", soma);
+	if(t == 'M') printf("%.1lf\n", soma / cont);
+
+	return 0;
+}
